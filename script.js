086032fa4e6d971fd3494313a97499fba7f8d8b5
Sheridan <sheridanmaustad@gmail.com>
2021-05-20 03:27:57 -0600

    Work and progress

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+// const h2 = document.createElement('h2');
+// h2.textContent = "This content added by JavaScript"; 
+// document.querySelector('body').appendChild(h2); 
+
+const btn = document.getElementById("btn")
+
+const catUrl = 
+fetch("https://thatcopy.pw/catapi/rest/")
+	.then(res => res.json())
+	console.log(catUrl)
+
